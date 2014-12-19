2c5dd8786eb81f34331f57939519a33bca032c6a
Qawelesizwe <qawemlilo@gmail.com>
2014-12-19 10:35:19 +0200

    Added status codes to responses

--- app.js
+++ app.js
@@ -147,163 +147,170 @@ All is set, now we can go ahead and start setting up our api routes. First up we
 **/
 
 
-    router.route('/users')
-      // fetch all users
-      .get(function (req, res) {
-        Users.forge()
-        .fetch()
-        .then(function (collection) {
-          res.json({error: false, data: collection.toJSON()});
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
-      })
+router.route('/users')
+  // fetch all users
+  .get(function (req, res) {
+    Users.forge()
+    .fetch()
+    .then(function (collection) {
+      res.json({error: false, data: collection.toJSON()});
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  })
 
-      // create a user
-      .post(function (req, res) {
-        User.forge({
-          name: req.body.name,
-          email: req.body.email
-        })
-        .save()
-        .then(function (user) {
-          res.json({error: false, data: {id: user.get('id')}});
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        }); 
+  // create a user
+  .post(function (req, res) {
+    User.forge({
+      name: req.body.name,
+      email: req.body.email
+    })
+    .save()
+    .then(function (user) {
+      res.json({error: false, data: {id: user.get('id')}});
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    }); 
+  });
+
+router.route('/users/:id')
+  // fetch user
+  .get(function (req, res) {
+    User.forge({id: req.params.id})
+    .fetch()
+    .then(function (user) {
+      if (!user) {
+        res.status(404).json({error: true, data: {}});
+      }
+      else {
+        res.json({error: false, data: user.toJSON()});
+      }
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  })
+
+  // update user details
+  .put(function (req, res) {
+    User.forge({id: req.params.id})
+    .fetch({require: true})
+    .then(function (user) {
+      user.save({
+        name: req.body.name || user.get('name'),
+        email: req.body.email || user.get('email')
+      })
+      .then(function () {
+        res.json({error: false, data: {message: 'User details updated'}});
+      })
+      .otherwise(function (err) {
+        res.status(500).json({error: true, data: {message: err.message}});
       });
-    
-    router.route('/users/:id')
-      // fetch user
-      .get(function (req, res) {
-        Users.forge({id: req.params.id})
-        .fetch()
-        .then(function (user) {
-          res.json({error: false, data: user.toJSON()});
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
-      })
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  })
 
-      // update user details
-      .put(function (req, res) {
-        User.forge({id: req.params.id})
-        .fetch()
-        .then(function (user) {
-          user.save({
-            name: req.body.name || user.get('name'),
-            email: req.body.email || user.get('email')
-          })
-          .then(function () {
-            res.json({error: false, data: {message: 'User details updated'}});
-          })
-          .otherwise(function (err) {
-            res.json({error: true, data: {message: err.message}});
-          });
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
+  // delete a user
+  .delete(function (req, res) {
+    User.forge({id: req.params.id})
+    .fetch({require: true})
+    .then(function (user) {
+      user.destroy()
+      .then(function () {
+        res.json({error: true, data: {message: 'User successfully deleted'}});
       })
-    
-      // delete a user
-      .delete(function (req, res) {
-        User.forge({id: req.params.id})
-        .fetch()
-        .then(function (user) {
-          user.destroy()
-          .then(function () {
-            res.json({error: true, data: {message: 'User successfully deleted'}});
-          })
-          .otherwise(function (err) {
-            res.json({error: true, data: {message: err.message}});
-          });
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
+      .otherwise(function (err) {
+        res.status(500).json({error: true, data: {message: err.message}});
       });
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  });
 
-/*
-Categories have a one-to-many relation with posts so it is also a good idea define route routes next.
-*/
 
-    router.route('/categories')
-      // fetch all categories
-      .get(function (req, res) {
-        Categories.forge()
-        .fetch()
-        .then(function (collection) {
-          res.json({error: false, data: collection.toJSON()});
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
+router.route('/categories')
+  // fetch all categories
+  .get(function (req, res) {
+    Categories.forge()
+    .fetch()
+    .then(function (collection) {
+      res.json({error: false, data: collection.toJSON()});
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  })
+
+  // create a new category
+  .post(function (req, res) {
+    Category.forge({name: req.body.name})
+    .save()
+    .then(function (category) {
+      res.json({error: false, data: {id: category.get('id')}});
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    }); 
+  });
+
+router.route('/categories/:id')
+  // fetch all categories
+  .get(function (req, res) {
+    Category.forge({id: req.params.id})
+    .fetch()
+    .then(function (category) {
+      if(!category) {
+        res.status(404).json({error: true, data: {}});
+      }
+      else {
+        res.json({error: false, data: category.toJSON()});
+      }
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  })   
+
+  // update a category
+  .put(function (req, res) {
+    Category.forge({id: req.params.id})
+    .fetch({require: true})
+    .then(function (category) {
+      category.save({name: req.body.name || category.get('name')})
+      .then(function () {
+        res.json({error: false, data: {message: 'Category updated'}});
       })
-
-      // create a new category
-      .post(function (req, res) {
-        Category.forge({name: req.body.name})
-        .save()
-        .then(function (category) {
-          res.json({error: false, data: {id: category.get('id')}});
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        }); 
+      .otherwise(function (err) {
+        res.status(500).json({error: true, data: {message: err.message}});
       });
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  })
 
-    router.route('/categories/:id')
-      // fetch all categories
-      .get(function (req, res) {
-        Category.forge({id: req.params.id})
-        .fetch()
-        .then(function (category) {
-          res.json({error: false, data: category.toJSON()});
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
-      })   
-
-      // update a category
-      .put(function (req, res) {
-        Category.forge({id: req.params.id})
-        .fetch()
-        .then(function (category) {
-          category.save({name: req.body.name || category.get('name')})
-          .then(function () {
-            res.json({error: false, data: {message: 'Category updated'}});
-          })
-          .otherwise(function (err) {
-            res.json({error: true, data: {message: err.message}});
-          });
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
+  // delete a category
+  .delete(function (req, res) {
+    Category.forge({id: req.params.id})
+    .fetch({require: true})
+    .then(function (category) {
+      category.destroy()
+      .then(function () {
+        res.json({error: true, data: {message: 'Category successfully deleted'}});
       })
-    
-      // delete a category
-      .delete(function (req, res) {
-        Category.forge({id: req.params.id})
-        .fetch()
-        .then(function (category) {
-          category.destroy()
-          .then(function () {
-            res.json({error: true, data: {message: 'Category successfully deleted'}});
-          })
-          .otherwise(function (err) {
-            res.json({error: true, data: {message: err.message}});
-          });
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
+      .otherwise(function (err) {
+        res.status(500).json({error: true, data: {message: err.message}});
       });
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  });
 
 
 function saveTags(tags) {
@@ -346,113 +353,119 @@ function saveTags(tags) {
 }
 
 
-    router.route('/posts')
-      // fetch all posts
-      .get(function (req, res) {
-        Posts.forge()
-        .fetch()
-        .then(function (collection) {
-          res.json({error: false, data: collection.toJSON()});
+router.route('/posts')
+  // fetch all posts
+  .get(function (req, res) {
+    Posts.forge()
+    .fetch()
+    .then(function (collection) {
+      res.json({error: false, data: collection.toJSON()});
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  });
+
+router.route('/posts/:id')
+  // fetch a post by id
+  .get(function (req, res) {
+    Post.forge({id: req.params.id})
+    .fetch({withRelated: ['categories', 'tags']})
+    .then(function (post) {
+      if (!post) {
+        res.status(404).json({error: true, data: {}});
+      }
+      else {
+        res.json({error: false, data: post.toJSON()});
+      }
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  });
+
+
+
+router.route('/posts')
+
+  .post(function (req, res) {
+    var tags = req.body.tags;
+   
+   // parse tags variable
+    if (tags) {
+      tags = tags.split(',').map(function (tag){
+        return tag.trim();
+      });
+    }
+    else {
+      tags = ['uncategorised'];
+    }
+    
+    // save post variables
+    Post.forge({
+      user_id: req.body.user_id,
+      category_id: req.body.category_id,
+      title: req.body.title,
+      slug: req.body.title.replace(/ /g, '-').toLowerCase(),
+      html: req.body.post
+    })
+    .save()
+    .then(function (post) {
+      
+      // post successfully saved
+      // save tags
+      saveTags(tags)
+      .then(function (ids) {
+
+        post.load(['tags'])
+        .then(function (model) {
+         
+          // attach tags to post
+          model.tags().attach(ids);
+
+          res.json({error: false, data: {message: 'Tags saved'}});
         })
         .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
+          res.status(500).json({error: true, data: {message: err.message}});
         });
-      });
-    
-    router.route('/posts/:id')
-      // fetch a post by id
-      .get(function (req, res) {
-        Post.forge({id: req.params.id})
-        .fetch({withRelated: ['category', 'tags']})
-        .then(function (post) {
-          res.json({error: false, data: post.toJSON()});
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
-      });
+      })
+      .otherwise(function (err) {
+        res.status(500).json({error: true, data: {message: err.message}}); 
+      });      
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    }); 
+  });
 
 
-    router.route('/posts')
-      .post(function (req, res) {
-        var tags = req.body.tags;
-       
-       // parse tags variable
-        if (tags) {
-          tags = tags.split(',').map(function (tag){
-            return tag.trim();
-          });
-        }
-        else {
-          tags = ['uncategorised'];
-        }
-        
-        // save post variables
-        Post.forge({
-          user_id: req.body.user_id,
-          category_id: req.body.category_id,
-          title: req.body.title,
-          slug: req.body.title.replace(/ /g, '-').toLowerCase(),
-          html: req.body.post
-        })
-        .save()
-        .then(function (post) {
-          
-          // post successfully saved
-          // save tags
-          saveTags(tags)
-          .then(function (ids) {
-    
-            post.load(['tags'])
-            .then(function (model) {
-             
-              // attach tags to post
-              model.tags().attach(ids);
-    
-              res.json({error: false, data: {message: 'Tags saved'}});
-            })
-            .otherwise(function (err) {
-              res.json({error: true, data: {message: err.message}});
-            });
-          })
-          .otherwise(function (err) {
-            res.json({error: true, data: {message: err.message}}); 
-          });      
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        }); 
-      });
+router.route('/posts/category/:id')
+  .get(function (req, res) {
+    Category.forge({id: req.params.id})
+    .fetch({withRelated: ['posts']})
+    .then(function (category) {
+      var posts = category.related('posts');
 
+      res.json({error: false, data: posts.toJSON()});
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  });
 
+router.route('/posts/tag/:slug')
+  .get(function (req, res) {
+    Tag.forge({slug: req.params.slug})
+    .fetch({withRelated: ['posts']})
+    .then(function (tag) {
+      var posts = tag.related('posts');
 
-    router.route('/posts/category/:id')
-      .get(function (req, res) {
-        Category.forge({id: req.params.id})
-        .fetch({withRelated: ['posts'], require: true})
-        .then(function (category) {
-          var posts = category.related('posts');
-    
-          res.json({error: false, data: posts.toJSON()});
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
-      });
-    
-    router.route('/posts/tag/:slug')
-      .get(function (req, res) {
-        Tag.forge({slug: req.params.slug})
-        .fetch({withRelated: ['posts'], require: true})
-        .then(function (tag) {
-          var posts = tag.related('posts');
-    
-          res.json({error: false, data: posts.toJSON()});
-        })
-        .otherwise(function (err) {
-          res.json({error: true, data: {message: err.message}});
-        });
-      });
+      res.json({error: false, data: posts.toJSON()});
+    })
+    .otherwise(function (err) {
+      res.status(500).json({error: true, data: {message: err.message}});
+    });
+  });
 
 
 app.use('/api', router);
